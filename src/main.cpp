4204dad5d92970c75bef5334b6efc17312ad524b
Hemish Patel <Hemishpatel2005@gmail.com>
2026-08-18 20:28:08 +1200

    Added initial Dual Servo Control code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,46 @@
 #include <Arduino.h>
+#include <ESP32Servo.h>
 
-// put function declarations here:
-int myFunction(int, int);
+Servo servo1;
+Servo servo2;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+const int SERVO1_PIN = 18;
+const int SERVO2_PIN = 19;
+
+void setup()
+{
+  servo1.attach(SERVO1_PIN);
+  servo2.attach(SERVO2_PIN);
+
+  // Start both servos at 0 degrees
+  servo1.write(0);
+  servo2.write(0);
+
+  delay(1000);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Move from 0 to 180 degrees
+  for (int angle = 0; angle <= 180; angle++)
+  {
+    servo1.write(angle);
+    servo2.write(angle);
+
+    delay(15);
+  }
+
+  delay(500);
+
+  // Move from 180 back to 0 degrees
+  for (int angle = 180; angle >= 0; angle--)
+  {
+    servo1.write(angle);
+    servo2.write(angle);
+
+    delay(15);
+  }
+
+  delay(500);
 }
